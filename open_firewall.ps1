304d84ccfb45b6337422255c1f83d5f20c83c8bf
Fernando W.P.S. <it24103815@my.sliit.lk>
2026-05-04 13:21:14 +0530

    Delete open_firewall.ps1

--- open_firewall.ps1
+++
@@ -1,3 +0,0 @@
-netsh advfirewall firewall add rule name="FitTrack Backend Port 5000" dir=in action=allow protocol=TCP localport=5000
-Write-Host "✅ Port 5000 opened in Windows Firewall!" -ForegroundColor Green
-Pause
